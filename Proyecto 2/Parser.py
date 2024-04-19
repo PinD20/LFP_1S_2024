31cf487c44cde735eb09ae0ecb6d4b42cbdfb447
PinD20 <enrique.pinula@gmail.com>
2024-04-19 12:23:17 -0600

    [ADD] Parser

--- Proyecto 2/Parser.py
+++ Proyecto 2/Parser.py
@@ -6,6 +6,7 @@ class Parser():
 
         #Controlar fin de tokens
         self.tokens.append(Token("EOF", "EOF", -1, -1))
+        #End of file = EOF
 
 
     def parse(self):
@@ -15,6 +16,7 @@ class Parser():
     def inicio(self):
         self.elemento()
         self.otro_elemento()
+        print("Análisis sintáctico exitoso")
 
     #<elemento> ::= tk_llaveA <instruccionID> <instruccionER> <instruccionCadenas> tk_llaveC
     def elemento(self):
@@ -31,3 +33,143 @@ class Parser():
                 print("error, Se esperaba una llave de cierre")
         else:
             print("Error, se esperaba una llave de apertura")
+
+    # <otro_elemento> ::= tk_coma <elemento> <otro_elemento>
+    #                  | epsilon
+    def otro_elemento(self):
+        if self.tokens[0].name == "tk_coma":
+            self.tokens.pop(0)
+            self.elemento()
+            self.otro_elemento()
+        else:
+            pass #Se acepta épsilon
+
+    #<instruccionID> ::= tk_id tk_dosPuntos tk_entero tk_PyC
+    def instruccionID(self):
+        if self.tokens[0].name == "tk_id":
+            self.tokens.pop(0)
+            if self.tokens[0].name == "tk_dosPuntos":
+                self.tokens.pop(0)
+                if self.tokens[0].name == "tk_entero":
+                    self.tokens.pop(0)
+                    if self.tokens[0].name == "tk_PyC":
+                        self.tokens.pop(0)
+                    else:
+                        print("Error, se esperaba ';'")
+                else:
+                    print("Error, se esperaba un entero")
+            else:
+                print("Error, se esperaba ':'")
+        else:
+            print("Error, se esperaba la palabra reservada 'ID'")
+
+    #<instruccionER> ::= tk_ER tk_dosPuntos <expresion> <otraExpresion> tk_PyC
+    def instruccionER(self):
+        if self.tokens[0].name == "tk_ER":
+            self.tokens.pop(0)
+            if self.tokens[0].name == "tk_dosPuntos":
+                self.tokens.pop(0)
+
+                self.expresion()
+                self.otraExpresion()
+
+                if self.tokens[0].name == "tk_PyC":
+                    self.tokens.pop(0)
+                else:
+                    print("Error, se esperaba ';' y se obtuvo" + self.tokens[0].name)
+            else:
+                print("Error, se esperaba ':'")
+        else:
+            print("Error, se esperaba la palabra reservada 'ER'")
+
+    #<expresion> ::= parA <expresion> parC <operador>
+    #              | <elementoER> <operador>
+    def expresion(self):
+        if self.tokens[0].name == "tk_parA":
+            self.tokens.pop(0)
+
+            self.expresion()
+
+            if self.tokens[0].name == "tk_parC":
+                self.tokens.pop(0)
+
+                self.operador()
+            else:
+                print("Error, se esperaba un parentesis de cierre")
+        else:
+            self.elementoER()
+            self.operador()
+
+    #<otraExpresion> ::= <expresion> <otraExpresion>
+    #                  | epsilon
+    def otraExpresion(self):
+        if self.tokens[0].name == "tk_parA" or self.tokens[0].name == "tk_cadena" or self.tokens[0].name == "tk_entero" or self.tokens[0].name == "tk_decimal":
+            self.expresion()
+            self.otraExpresion()
+        else:
+            pass #Se acepta épsilon
+
+    #<operador> ::= <operadorUnario>
+    #             | tk_Or <expresion>
+    #             | epsilon
+    def operador(self):
+        if self.tokens[0].name == "tk_Mas" or self.tokens[0].name == "tk_Asterisco" or self.tokens[0].name == "tk_Interrogacion":
+            self.operadorUnario()
+        elif self.tokens[0].name == "tk_Or":
+            self.tokens.pop(0)
+            self.expresion()
+        else:
+            pass# Se acepta épsilon
+
+    # <operadorUnario> ::= tk_Mas
+    #                    | tk_Asterisco
+    #                    | tk_interrogacion
+    def operadorUnario(self):
+        if self.tokens[0].name == "tk_Mas" or self.tokens[0].name == "tk_Asterisco" or self.tokens[0].name == "tk_Interrogacion":
+            self.tokens.pop(0)
+        else:
+            print("Error, se esperaba un operador unario")
+
+    # <elementoER> ::= tk_cadena
+    #                | tk_entero
+    #                | tk_decimal
+    def elementoER(self):
+        if self.tokens[0].name == "tk_cadena" or self.tokens[0].name == "tk_entero" or self.tokens[0].name == "tk_decimal":
+            self.tokens.pop(0)
+        else:
+            print("Error, se esperaba cadena, entero o decimal")
+
+    #<instruccionCadenas> ::= tk_Cadenas tk_dosPuntos tk_cadena <otraCadena> tk_PyC
+    def instruccionCadenas(self):
+        if self.tokens[0].name == "tk_Cadenas":
+            self.tokens.pop(0)
+            if self.tokens[0].name == "tk_dosPuntos":
+                self.tokens.pop(0)
+                if self.tokens[0].name == "tk_cadena":
+                    self.tokens.pop(0)
+
+                    self.otraCadena()
+
+                    if self.tokens[0].name == "tk_PyC":
+                        self.tokens.pop(0)
+                    else:
+                        print("Error, se esperaba ';' y se obtuvo " + self.tokens[0].name)
+                else:
+                    print("Error, se esperaba una cadena")
+            else:
+                print("Error, se esperaba ':'")
+        else:
+            print("Error, se esperaba la palabra reservada 'Cadenas'")
+
+    # <otraCadena> ::= tk_coma tk_cadena <otraCadena>
+    #                | epsilon
+    def otraCadena(self):
+        if self.tokens[0].name == "tk_coma":
+            self.tokens.pop(0)
+            if self.tokens[0].name == "tk_cadena":
+                self.tokens.pop(0)
+                self.otraCadena()
+            else:
+                print("Error, se esperaba una cadena y se obtuvo" + self.tokens[0].name)
+        else:
+            pass #Se acepta épsilon
---
+++ Proyecto 2/main.py
@@ -0,0 +1,49 @@
+from Parser import Parser
+from Token import Token
+
+'''
+============= Entrada =================
+
+{
+    ID: 1;
+    ER: "abc"*(8|20)?1;
+    CADENAS: "abc201", "81";
+}
+
+'''
+
+def llenarLista():
+    tokens = []
+    tokens.append(Token("tk_llaveA", "{", 1, 1))
+    tokens.append(Token("tk_id", "ID", 1, 1))
+    tokens.append(Token("tk_dosPuntos", ":", 1, 1))
+    tokens.append(Token("tk_entero", "1", 1, 1))
+    tokens.append(Token("tk_PyC", ";", 1, 1))
+    tokens.append(Token("tk_ER", "ER", 1, 1))
+    tokens.append(Token("tk_dosPuntos", ":", 1, 1))
+    tokens.append(Token("tk_cadena", "\"abc\"", 1, 1))
+    tokens.append(Token("tk_Asterisco", "*", 1, 1))
+    tokens.append(Token("tk_parA", "(", 1, 1))
+    tokens.append(Token("tk_entero", "8", 1, 1))
+    tokens.append(Token("tk_Or", "|", 1, 1))
+    tokens.append(Token("tk_entero", "20", 1, 1))
+    tokens.append(Token("tk_parC", ")", 1, 1))
+    tokens.append(Token("tk_Interrogacion", "?", 1, 1))
+    tokens.append(Token("tk_entero", "1", 1, 1))
+    tokens.append(Token("tk_PyC", ";", 1, 1))
+    tokens.append(Token("tk_Cadenas", "CADENAS", 1, 1))
+    tokens.append(Token("tk_dosPuntos", ":", 1, 1))
+    tokens.append(Token("tk_cadena", "\"abc201\"", 1, 1))
+    tokens.append(Token("tk_coma", ",", 1, 1))
+    tokens.append(Token("tk_cadena", "\"81\"", 1, 1))
+    tokens.append(Token("tk_PyC", ";", 1, 1))
+    tokens.append(Token("tk_llaveC", "}", 1, 1))
+
+    return tokens
+
+if __name__ == "__main__":
+    #Se crea el objeto parser
+    parser = Parser(llenarLista())
+    
+    #Se llama al método parse
+    parser.parse()
